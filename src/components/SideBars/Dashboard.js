5bbce469965e675234cbb0a7f92355c015747983
SadieTq <saadtqshk@gmail.com>
2024-10-27 19:29:52 +0500

    Dashboard Update

--- src/components/SideBars/Dashboard.js
+++ src/components/SideBars/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { PieChart, Pie, Cell, Tooltip, ResponsiveContainer } from "recharts";
-import { Spin, Divider, Row, Col, Card, Button } from "antd";
+import { Spin, Divider, Row, Col } from "antd";
 import SurveyData from "../SideBars/SurveyData";
 
 const COLORS = [
@@ -19,7 +19,6 @@ function Dashboard() {
   const [surveyData, setSurveyData] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  // Function to fetch survey data
   const fetchSurveyData = async () => {
     setLoading(true);
     try {
@@ -27,7 +26,7 @@ function Dashboard() {
       const data = await response.json();
       setSurveyData(data);
     } catch (error) {
-      
+      console.error("Error fetching survey data:", error);
     } finally {
       setLoading(false);
     }
@@ -37,33 +36,20 @@ function Dashboard() {
     fetchSurveyData();
   }, []);
 
-  // Define data for each chart category
-  const vaccinationData = surveyData
-    ? [
-        // { name: "Total Vaccinated Coverage", value: surveyData.total },
-        { name: "Total AFP cases", value: surveyData["Total AFP Case"] },
-        { name: "Zero Dose count", value: surveyData["Total Zero Dose Count"] },
-        { name: "UCMO Percentage", value: surveyData["ucmo %"] },
-        { name: "AIC Percentage", value: surveyData["aic %"] },
-      ]
-    : [];
-
   const teamData = surveyData
     ? [
         { name: "Team login before 8:30", value: surveyData["before 8:30"] },
         { name: "Team login after 8:30", value: surveyData["after 8:30"] },
-        {
-          name: "Team revisited after 2pm",
-          value: surveyData.visitsAfter2PMCount,
-        },
+        
       ]
     : [];
 
   const housesData = surveyData
     ? [
-        { name: "Locked houses", value: surveyData.uniqueLockedHouseCount },
-
-        { name: "Revisited houses", value: surveyData.revisitedHouseData },
+        // { name: "Total NA Houses", value: surveyData["Total NA houses"] },
+        { name: "NA house cover same day", value: surveyData["NA house cover same day "] },
+        { name: "Remaining Na House", value: surveyData["Remaining Na House "] },
+        { name: "Na house visited after 2 pm", value: surveyData["Na housevisited after 2 pm "] },
       ]
     : [];
 
@@ -72,61 +58,31 @@ function Dashboard() {
         { name: "School children", value: surveyData.school },
         { name: "Street children", value: surveyData.street },
         { name: "Guest children", value: surveyData.guestChild },
+        { name: "In-house Children", value: surveyData["Children vaccinated at House"] },
         { name: "Newborn Count", value: surveyData["Total Newborn Count"] },
-        // {
-        //   name: "Total vaccinated in house",
-        //   value: surveyData.availableChild,
-        // },
       ]
     : [];
 
   const refusalsData = surveyData
     ? [
-        // {
-        //   name: "Total refusals",
-        //   value: surveyData.refusalStats.totalRefusalCount,
-        // },
-        {
-          name: "Religious refusals",
-          value: surveyData.refusalStats.refusalReasons.religiousRefusal,
-        },
-      
-        {
-          name: "Medical refusals",
-          value: surveyData.refusalStats.refusalReasons.medicalRefusal,
-        },
-  {
-          name: "Other refusals",
-          value: surveyData.refusalStats.refusalReasons.otherRefusal,
-        },
-
-        {
-          name: "Unknown refusals",
-          value: surveyData.refusalStats.refusalReasons.unknown,
-        },
+        // { name: "Total refusals", value: surveyData.refusalStats.totalRefusalCount },
+        { name: "Religious refusals", value: surveyData.refusalStats.refusalReasons.religiousRefusal },
+        { name: "Medical refusals", value: surveyData.refusalStats.refusalReasons.medicalRefusal },
+        { name: "Other refusals", value: surveyData.refusalStats.refusalReasons.otherRefusal },
+        { name: "Unknown refusals", value: surveyData.refusalStats.refusalReasons.unknown },
       ]
     : [];
 
   const nadata = surveyData
     ? [
-        {
-          name: "NA Children Same Day covered",
-          value: surveyData["covered NA Children same day"],
-        },
-        
-        { name: "NA Children", value: surveyData.uniqueNAChildrenCount },
-        {
-          name: "NA same day (visited)",
-          value: surveyData["Na Housenot visted same day"],
-        },
-        {
-          name: "NA same day (not visited)",
-          value: surveyData["Na Housenot Not visted same day"],
-        },
+        // { name: "Total NA", value: surveyData["Total Na "] },
+        { name: "NA Covered Same Day", value: surveyData["Cover same day Na "] },
+        { name: "Remaining NA", value: surveyData["Remaining Na "] },
+        { name: "Not covered after 2pm", value: surveyData["Not cover after 2pm day"] },
+        { name: "Covered after 2pm", value: surveyData["NA cover after 2 pm "] },
       ]
     : [];
 
-  // Render individual pie charts with color-coded legend and values
   const renderPieChartWithLegend = (data, colorIndex) => (
     <Row align="middle">
       <Col span={6}>
@@ -170,10 +126,7 @@ function Dashboard() {
               label
             >
               {data.map((entry, index) => (
-                <Cell
-                  key={`cell-${index}`}
-                  fill={COLORS[(colorIndex + index) % COLORS.length]}
-                />
+                <Cell key={`cell-${index}`} fill={COLORS[(colorIndex + index) % COLORS.length]} />
               ))}
             </Pie>
             <Tooltip />
@@ -183,26 +136,20 @@ function Dashboard() {
     </Row>
   );
 
-
-
   return (
     <div className="tab-panel">
+      <div className="form-container">
+        <h2>District Health Authority Lahore</h2>
+        <div style={{ display: 'flex' }}>
+          <h3 style={{ marginRight: '20px' }}>District: Lahore</h3>
+          <h3>Division: Lahore</h3>
+        </div>  
+      </div>
 
-       <div className="form-container">
-         <h2>District Health Authority Lahore</h2> 
-      <div style={{ display: 'flex' }}>
-         
-    <h3 style={{ marginRight: '20px' }}>District: Lahore</h3>
-    <h3>Division: Lahore</h3>
-</div>  
-
-       </div>
-
-       <Divider style={{ marginTop: "10px", marginBottom: "10px" }} />
-<div className="form-container">
-<SurveyData/>
-</div>
-
+      <Divider style={{ marginTop: "10px", marginBottom: "10px" }} />
+      <div className="form-container">
+        <SurveyData />
+      </div>
 
       <Divider style={{ marginTop: "20px", marginBottom: "20px" }} />
       <div className="form-container">
@@ -212,41 +159,41 @@ function Dashboard() {
           <Spin tip="Loading data..." />
         ) : (
           <>
-            {/* Render Pie Charts */}
             <Row gutter={[16, 16]}>
-              <Col span={12}>
-                <h3>Vaccination Data</h3>
-                {renderPieChartWithLegend(vaccinationData, 0)}
-              </Col>
               <Col span={12}>
                 <h3>Team Data</h3>
-                {renderPieChartWithLegend(teamData, 1)}
+                {renderPieChartWithLegend(teamData, 0)}
+              </Col>
+              <Col span={12}>
+                <h3>NA Houses Data</h3>
+                {renderPieChartWithLegend(housesData, 1)}
               </Col>
             </Row>
             <Row gutter={[16, 16]}>
-              <Col span={12}>
-                <h3>Houses Data</h3>
-                {renderPieChartWithLegend(housesData, 2)}
-              </Col>
               <Col span={12}>
                 <h3>Children Data</h3>
-                {renderPieChartWithLegend(childrenData, 3)}
+                {renderPieChartWithLegend(childrenData, 2)}
+              </Col>
+             <Col span={12}>
+                <h3>NA Children Data</h3>
+                {renderPieChartWithLegend(nadata, 4)}
               </Col>
             </Row>
             <Row gutter={[16, 16]}>
-              <Col span={12}>
+              
+
+
+ <Col span={12}>
                 <h3>Refusals Data</h3>
-                {renderPieChartWithLegend(refusalsData, 4)}
+                {renderPieChartWithLegend(refusalsData, 3)}
               </Col>
-              <Col span={12}>
-                <h3>NA Data</h3>
-                {renderPieChartWithLegend(nadata, 4)}
-              </Col>
+
+
+
             </Row>
           </>
         )}
       </div>
-      
     </div>
   );
 }
--- src/components/SideBars/SurveyData.js
+++ src/components/SideBars/SurveyData.js
@@ -1,113 +1,318 @@
-import React, { useEffect, useState } from 'react';
-import { Card, Button, Divider } from 'antd';
+import React, { useEffect, useState } from "react";
+import { Card, Button, Divider } from "antd";
 
 const SurveyData = () => {
   const [data, setData] = useState(null);
 
   useEffect(() => {
     // Fetch data from the API
-    fetch('http://110.38.226.9:4000/api/survey')
-      .then(response => response.json())
-      .then(json => setData(json))
-      .catch(error => console.error('Error fetching data:', error));
+    fetch("http://110.38.226.9:4000/api/survey")
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
   const cardTextStyle = {
-    fontSize: '14px', // Reducing font size for more compact text
-    lineHeight: '1.2', // Reducing line height for closer line spacing
+    fontSize: "14px", // Reducing font size for more compact text
+    lineHeight: "1.2", // Reducing line height for closer line spacing
   };
 
   return (
-    <div style={{ padding: '20px' }}>
+    <div style={{ padding: "20px" }}>
       <h3>Survey Data</h3>
-      <div style={{ display: 'flex', flexWrap: 'wrap', gap: '16px' }}>
+      <div style={{ display: "flex", flexWrap: "wrap", gap: "16px" }}>
         {/* Vaccination Data Card */}
         <Card
           title="Vaccination Data"
           extra={<Button type="primary">Details</Button>}
-          style={{ flex: '1 1 calc(33.333% - 16px)' }}
+          style={{ flex: "1 1 calc(33.333% - 16px)" }}
           bordered
         >
-          <p style={cardTextStyle}><span style={{ color: '#0088FE', fontWeight: 'bold', fontSize: '18px' }}>●</span> Total Vaccinated Coverage: <strong>{data ? data.total : 'Loading...'}</strong></p>
-          <Divider style={{ marginTop: "5px", marginBottom: "5px" }} />
-          <p style={cardTextStyle}><span style={{ color: '#00C49F', fontWeight: 'bold', fontSize: '18px' }}>●</span> Total AFP cases: <strong>{data ? data['Total AFP Case'] : 'Loading...'}</strong></p>
-          <p style={cardTextStyle}><span style={{ color: '#FFBB28', fontWeight: 'bold', fontSize: '18px' }}>●</span> Zero Dose count: <strong>{data ? data['Total Zero Dose Count'] : 'Loading...'}</strong></p>
-          <p style={cardTextStyle}><span style={{ color: '#FFBB28', fontWeight: 'bold', fontSize: '18px' }}>●</span> UCMO Percentage: <strong>{data ? data['ucmo %'] : 'Loading...'}</strong></p>
-          <p style={cardTextStyle}><span style={{ color: '#FFBB28', fontWeight: 'bold', fontSize: '18px' }}>●</span> AIC Pecentage: <strong>{data ? data['aic %'] : 'Loading...'}</strong></p>
-
-        </Card>
+          <p style={cardTextStyle}>
+            <span
+              style={{ color: "#0088FE", fontWeight: "bold", fontSize: "18px" }}
+            >
+              ●
+            </span>{" "}
+            Total Vaccinated Coverage:{" "}
+            <strong>{data ? data.total : "Loading..."}</strong>
+          </p>
+          
+   </Card>
 
         {/* Team Data Card */}
         <Card
           title="Team Data"
           extra={<Button type="primary">Details</Button>}
-          style={{ flex: '1 1 calc(33.333% - 16px)' }}
+          style={{ flex: "1 1 calc(33.333% - 16px)" }}
           bordered
         >
-          <p style={cardTextStyle}><span style={{ color: '#00C49F', fontWeight: 'bold', fontSize: '18px' }}>●</span> Team login before 8:30: <strong>{data ? data['before 8:30'] : 'Loading...'}</strong></p>
-          <Divider style={{ marginTop: "5px", marginBottom: "5px" }} />
-          <p style={cardTextStyle}><span style={{ color: '#FFBB28', fontWeight: 'bold', fontSize: '18px' }}>●</span> Team login after 8:30: <strong>{data ? data['after 8:30'] : 'Loading...'}</strong></p>
-          <p style={cardTextStyle}><span style={{ color: '#FF8042', fontWeight: 'bold', fontSize: '18px' }}>●</span> Team revisited after 2pm: <strong>{data ? data.visitsAfter2PMCount : 'Loading...'}</strong></p>
+          <p style={cardTextStyle}>
+            <span
+              style={{ color: "#00C49F", fontWeight: "bold", fontSize: "18px" }}
+            >
+              ●
+            </span>{" "}
+            Team login before 8:30:{" "}
+            <strong>{data ? data["before 8:30"] : "Loading..."}</strong>
+          </p>
+          {/* <Divider style={{ marginTop: "5px", marginBottom: "5px" }} /> */}
+          <p style={cardTextStyle}>
+            <span
+              style={{ color: "#FFBB28", fontWeight: "bold", fontSize: "18px" }}
+            >
+              ●
+            </span>{" "}
+            Team login after 8:30:{" "}
+            <strong>{data ? data["after 8:30"] : "Loading..."}</strong>
+          </p>
+          {/* <p style={cardTextStyle}><span style={{ color: '#FF8042', fontWeight: 'bold', fontSize: '18px' }}>●</span> Team revisited after 2pm: <strong>{data ? data.visitsAfter2PMCount : 'Loading...'}</strong></p> */}
         </Card>
 
         {/* Houses Data Card */}
         <Card
-          title="Houses Data"
+          title="NA Houses Data"
           extra={<Button type="primary">Details</Button>}
-          style={{ flex: '1 1 calc(33.333% - 16px)' }}
+          style={{ flex: "1 1 calc(33.333% - 16px)" }}
           bordered
         >
-          <p style={cardTextStyle}><span style={{ color: '#FFBB28', fontWeight: 'bold', fontSize: '18px' }}>●</span> Locked houses: <strong>{data ? data.uniqueLockedHouseCount : 'Loading...'}</strong></p>
-          <Divider style={{ marginTop: "5px", marginBottom: "5px" }} />
-          <p style={cardTextStyle}><span style={{ color: '#FF8042', fontWeight: 'bold', fontSize: '18px' }}>●</span> Revisited houses: <strong>{data ? data.revisitedHouseData : 'Loading...'}</strong></p>
+          <p style={cardTextStyle}>
+            <span
+              style={{ color: "#FFBB28", fontWeight: "bold", fontSize: "18px" }}
+            >
+              ●
+            </span>{" "}
+            Total NA Houses:{" "}
+            <strong>{data ? data["Total NA houses"] : "Loading..."}</strong>
+          </p>
+          {/* <Divider style={{ marginTop: "5px", marginBottom: "5px" }} /> */}
+          <p style={cardTextStyle}>
+            <span
+              style={{ color: "#FF8042", fontWeight: "bold", fontSize: "18px" }}
+            >
+              ●
+            </span>{" "}
+            NA house cover same day:{" "}
+            <strong>
+              {data ? data["NA house cover same day "] : "Loading..."}
+            </strong>
+          </p>
+          <p style={cardTextStyle}>
+            <span
+              style={{ color: "#FF8042", fontWeight: "bold", fontSize: "18px" }}
+            >
+              ●
+            </span>{" "}
+            Remaining Na House:{" "}
+            <strong>{data ? data["Remaining Na House "] : "Loading..."}</strong>
+          </p>
+          <p style={cardTextStyle}>
+            <span
+              style={{ color: "#FF8042", fontWeight: "bold", fontSize: "18px" }}
+            >
+              ●
+            </span>{" "}
+            Na housevisited after 2 pm:{" "}
+            <strong>
+              {data ? data["Na housevisited after 2 pm "] : "Loading..."}
+            </strong>
+          </p>
         </Card>
 
         {/* Children Data Card */}
         <Card
           title="Children Data"
           extra={<Button type="primary">Details</Button>}
-          style={{ flex: '1 1 calc(33.333% - 16px)' }}
+          style={{ flex: "1 1 calc(33.333% - 16px)" }}
           bordered
         >
-                    <p style={cardTextStyle}><span style={{ color: '#00C49F', fontWeight: 'bold', fontSize: '18px' }}>●</span> Total vaccinated in house: <strong>{data ? data.availableChild : 'Loading...'}</strong></p>
+          {/* <Divider style={{ marginTop: "5px", marginBottom: "5px" }} /> */}
+          <p style={cardTextStyle}>
+            <span
+              style={{ color: "#FF8042", fontWeight: "bold", fontSize: "18px" }}
+            >
+              ●
+            </span>{" "}
+            School children:{" "}
+            <strong>{data ? data.school : "Loading..."}</strong>
+          </p>
+          <p style={cardTextStyle}>
+            <span
+              style={{ color: "#FF8042", fontWeight: "bold", fontSize: "18px" }}
+            >
+              ●
+            </span>{" "}
+            Newborn Count:{" "}
+            <strong>{data ? data["Total Newborn Count"] : "Loading..."}</strong>
+          </p>
 
-          <Divider style={{ marginTop: "5px", marginBottom: "5px" }} />
-                   <p style={cardTextStyle}><span style={{ color: '#FF8042', fontWeight: 'bold', fontSize: '18px' }}>●</span> School children: <strong>{data ? data.school : 'Loading...'}</strong></p>
-                   <p style={cardTextStyle}><span style={{ color: '#FF8042', fontWeight: 'bold', fontSize: '18px' }}>●</span> Newborn Count: <strong>{data ? data['Total Newborn Count'] : 'Loading...'}</strong></p>
-
-          <p style={cardTextStyle}><span style={{ color: '#AF19FF', fontWeight: 'bold', fontSize: '18px' }}>●</span> Street children: <strong>{data ? data.street : 'Loading...'}</strong></p>
-          <p style={cardTextStyle}><span style={{ color: '#FFBB28', fontWeight: 'bold', fontSize: '18px' }}>●</span> Guest children: <strong>{data ? data.guestChild : 'Loading...'}</strong></p>
+          <p style={cardTextStyle}>
+            <span
+              style={{ color: "#AF19FF", fontWeight: "bold", fontSize: "18px" }}
+            >
+              ●
+            </span>{" "}
+            Street children:{" "}
+            <strong>{data ? data.street : "Loading..."}</strong>
+          </p>
+          <p style={cardTextStyle}>
+            <span
+              style={{ color: "#FFBB28", fontWeight: "bold", fontSize: "18px" }}
+            >
+              ●
+            </span>{" "}
+            Guest children:{" "}
+            <strong>{data ? data.guestChild : "Loading..."}</strong>
+          </p>
+          <p style={cardTextStyle}>
+            <span
+              style={{ color: "#FFBB28", fontWeight: "bold", fontSize: "18px" }}
+            >
+              ●
+            </span>{" "}
+            Inhouse Children:{" "}
+            <strong>
+              {data ? data["Children vaccinated at House"] : "Loading..."}
+            </strong>
+          </p>
         </Card>
 
-        {/* Refusals Data Card */}
         <Card
+          title="NA Children Data"
+          extra={<Button type="primary">Details</Button>}
+          style={{ flex: "1 1 calc(33.333% - 16px)" }}
+          bordered
+        >
+          <p style={cardTextStyle}>
+            <span
+              style={{ color: "#AF19FF", fontWeight: "bold", fontSize: "18px" }}
+            >
+              ●
+            </span>{" "}
+            Total NA:{" "}
+            <strong>
+              {data ? data["Total Na "] : "Loading..."}
+            </strong>
+          </p>
+          {/* <Divider style={{ marginTop: "5px", marginBottom: "5px" }} /> */}
+          <p style={cardTextStyle}>
+            <span
+              style={{ color: "#FF8042", fontWeight: "bold", fontSize: "18px" }}
+            >
+              ●
+            </span>{" "}
+            NA Covered Same Day:{" "}
+            <strong>
+              {data ? data["Cover same day Na "] : "Loading..."}
+            </strong>
+          </p>
+          <p style={cardTextStyle}>
+            <span
+              style={{ color: "#32CD32", fontWeight: "bold", fontSize: "18px" }}
+            >
+              ●
+            </span>{" "}
+            Remaining NA:{" "}
+            <strong>
+              {data ? data["Remaining Na "] : "Loading..."}
+            </strong>
+          </p>
+          <p style={cardTextStyle}>
+            <span
+              style={{ color: "#6A5ACD", fontWeight: "bold", fontSize: "18px" }}
+            >
+              ●
+            </span>{" "}
+            Not covered after 2pm:{" "}
+            <strong>
+              {data ? data["Not cover after 2pm day"] : "Loading..."}
+            </strong>
+          </p>
+          <p style={cardTextStyle}>
+            <span
+              style={{ color: "#6A5ACD", fontWeight: "bold", fontSize: "18px" }}
+            >
+              ●
+            </span>{" "}
+            Covered after 2pm:{" "}
+            <strong>
+              {data ? data["NA cover after 2 pm "] : "Loading..."}
+            </strong>
+          </p>
+        </Card>
+
+
+ <Card
           title="Refusals Data"
           extra={<Button type="primary">Details</Button>}
-          style={{ flex: '1 1 calc(33.333% - 16px)' }}
+          style={{ flex: "1 1 calc(33.333% - 16px)" }}
           bordered
         >
-          <p style={cardTextStyle}><span style={{ color: '#AF19FF', fontWeight: 'bold', fontSize: '18px' }}>●</span> Total refusals: <strong>{data ? data.refusalStats.totalRefusalCount : 'Loading...'}</strong></p>
-          <Divider style={{ marginTop: "5px", marginBottom: "5px" }} />
-          <p style={cardTextStyle}><span style={{ color: '#FFD700', fontWeight: 'bold', fontSize: '18px' }}>●</span> Religious refusals: <strong>{data ? data.refusalStats.refusalReasons.religiousRefusal : 'Loading...'}</strong></p>
-          <p style={cardTextStyle}><span style={{ color: '#32CD32', fontWeight: 'bold', fontSize: '18px' }}>●</span> Other refusals: <strong>{data ? data.refusalStats.refusalReasons.otherRefusal : 'Loading...'}</strong></p>
-          <p style={cardTextStyle}><span style={{ color: '#6A5ACD', fontWeight: 'bold', fontSize: '18px' }}>●</span> Medical refusals: <strong>{data ? data.refusalStats.refusalReasons.medicalRefusal : 'Loading...'}</strong></p>
-          <p style={cardTextStyle}><span style={{ color: '#6A5ACD', fontWeight: 'bold', fontSize: '18px' }}>●</span> Unknown refusals: <strong>{data ? data.refusalStats.refusalReasons.unknown : 'Loading...'}</strong></p>
-      
+          <p style={cardTextStyle}>
+            <span
+              style={{ color: "#AF19FF", fontWeight: "bold", fontSize: "18px" }}
+            >
+              ●
+            </span>{" "}
+            Total refusals:{" "}
+            <strong>
+              {data ? data.refusalStats.totalRefusalCount : "Loading..."}
+            </strong>
+          </p>
+          {/* <Divider style={{ marginTop: "5px", marginBottom: "5px" }} /> */}
+          <p style={cardTextStyle}>
+            <span
+              style={{ color: "#FFD700", fontWeight: "bold", fontSize: "18px" }}
+            >
+              ●
+            </span>{" "}
+            Religious refusals:{" "}
+            <strong>
+              {data
+                ? data.refusalStats.refusalReasons.religiousRefusal
+                : "Loading..."}
+            </strong>
+          </p>
+          <p style={cardTextStyle}>
+            <span
+              style={{ color: "#32CD32", fontWeight: "bold", fontSize: "18px" }}
+            >
+              ●
+            </span>{" "}
+            Other refusals:{" "}
+            <strong>
+              {data
+                ? data.refusalStats.refusalReasons.otherRefusal
+                : "Loading..."}
+            </strong>
+          </p>
+          <p style={cardTextStyle}>
+            <span
+              style={{ color: "#6A5ACD", fontWeight: "bold", fontSize: "18px" }}
+            >
+              ●
+            </span>{" "}
+            Medical refusals:{" "}
+            <strong>
+              {data
+                ? data.refusalStats.refusalReasons.medicalRefusal
+                : "Loading..."}
+            </strong>
+          </p>
+          <p style={cardTextStyle}>
+            <span
+              style={{ color: "#6A5ACD", fontWeight: "bold", fontSize: "18px" }}
+            >
+              ●
+            </span>{" "}
+            Unknown refusals:{" "}
+            <strong>
+              {data ? data.refusalStats.refusalReasons.unknown : "Loading..."}
+            </strong>
+          </p>
         </Card>
 
-        {/* NA Data Card */}
-        <Card
-          title="NA Data"
-          extra={<Button type="primary">Details</Button>}
-          style={{ flex: '1 1 calc(33.333% - 16px)' }}
-          bordered
-        >
-          <p style={cardTextStyle}><span style={{ color: '#AF19FF', fontWeight: 'bold', fontSize: '18px' }}>●</span> NA Children Same Day covered: <strong>{data ? data['covered NA Children same day'] : 'Loading...'}</strong></p>
-          <Divider style={{ marginTop: "5px", marginBottom: "5px" }} />
-          <p style={cardTextStyle}><span style={{ color: '#FF8042', fontWeight: 'bold', fontSize: '18px' }}>●</span> NA Children: <strong>{data ? data['uniqueNAChildrenCount'] : 'Loading...'}</strong></p>
-          <p style={cardTextStyle}><span style={{ color: '#32CD32', fontWeight: 'bold', fontSize: '18px' }}>●</span> NA same day (visited): <strong>{data ? data['Na Housenot visted same day'] : 'Loading...'}</strong></p>
-          <p style={cardTextStyle}><span style={{ color: '#6A5ACD', fontWeight: 'bold', fontSize: '18px' }}>●</span> NA same day (not visited): <strong>{data ? data['Na Housenot Not visted same day'] : 'Loading...'}</strong></p>
-        </Card>
+
       </div>
     </div>
   );
